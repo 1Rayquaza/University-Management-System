756c1ea06e1d1c5aba2f28bb762df2ec7a596cc9
1Rayquaza <rishitabarnwal4096@gmail.com>
2023-06-28 23:43:40 +0530

    Added forms and middlewares

--- University_Management_System/urls.py
+++ University_Management_System/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
+    path("", include('main_app.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
